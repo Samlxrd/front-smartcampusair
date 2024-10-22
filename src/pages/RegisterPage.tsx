dfe276463bc362705ef1ad0a95048fd7400b71ab
Samlxrd <samlord.dzn@gmail.com>
2024-10-22 10:22:11 -0300

    correção estilos

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -2,7 +2,7 @@ import { RegisterForm } from "../components/register-form";
 
 export function RegisterPage() {
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div>
             <RegisterForm/>
         </div>
     )
